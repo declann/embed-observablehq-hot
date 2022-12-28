91fab94ce9a7e9b62504b37641c39d0ba6c582c4
Declan Naughton <declan.naughton2@mail.dcu.ie>
2022-12-28 14:48:20 -0300

    show models

--- src/Projection.js
+++ src/Projection.js
@@ -4,11 +4,13 @@ import notebook from "f0397f5d636d6705";
 
 function Notebook() {
   const projection_primeRef = useRef();
+  const modelsRef = useRef();
 
   useEffect(() => {
     const runtime = new Runtime();
     const m = runtime.module(notebook, name => {
       if (name === "projection_prime") return new Inspector(projection_primeRef.current);
+      if (name === "models") return new Inspector(modelsRef.current);
     });
     m.value("models").then(models => {
       m.redefine("models", [...models, {x:n=>n, y:n=>n,dy:n=>n,compressed:n=>true}])
@@ -21,6 +23,7 @@ function Notebook() {
   return (
     <>
       <div ref={projection_primeRef} />
+      <div ref={modelsRef} />
       <p>Credit: <a href="https://observablehq.com/d/f0397f5d636d6705@622">calculang bouncing ball model ⚽🏀🥎 by Declan Naughton</a></p>
     </>
   );
