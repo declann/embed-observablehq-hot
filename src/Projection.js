edfaf3881f72d7b366ff89fa3c5566ac884f8abb
Declan Naughton <declan.naughton2@mail.dcu.ie>
2022-12-28 15:05:45 -0300

    HMR of template working

--- src/Projection.js
+++ src/Projection.js
@@ -2,6 +2,10 @@ import React, {useRef, useEffect} from "react";
 import {Runtime, Inspector} from "@observablehq/runtime";
 import notebook from "f0397f5d636d6705";
 
+import model from './model.js';
+
+var m;
+
 // HMR for a model will be different ...
 
 function Notebook() {
@@ -10,13 +14,14 @@ function Notebook() {
 
   useEffect(() => {
     const runtime = new Runtime();
-    const m = runtime.module(notebook, name => {
+    m = runtime.module(notebook, name => {
       if (name === "projection_prime") return new Inspector(projection_primeRef.current);
       if (name === "models") return new Inspector(modelsRef.current);
     });
+    /*
     m.value("models").then(models => {
       m.redefine("models", [...models, {x:n=>n, y:n=>n,dy:n=>n,compressed:n=>true}])
-    })
+    })*/
     //console.log(t)
     
     return () => runtime.dispose();
@@ -29,6 +34,14 @@ function Notebook() {
       <p>Credit: <a href="https://observablehq.com/d/f0397f5d636d6705@622">calculang bouncing ball model ⚽🏀🥎 by Declan Naughton</a></p>
     </>
   );
+}
+
+if (module.hot) {
+  module.hot.accept('./model.js', () => {
+    m.value("models").then(models => {
+      m.redefine("models", [...models, require('./model.js').default])
+    })
+  })
 }
 
 export default Notebook;
---
+++ src/model.js
@@ -0,0 +1,3 @@
+const model = {x:n=>n, y:n=>n,dy:n=>n,compressed:n=>false} //
+
+export default model
